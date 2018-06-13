644a9c22adf749327e7451f75509d3c1703a2152
foundserveradmin <40034682+foundserveradmin@users.noreply.github.com>
2018-06-13 13:28:32 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -3312,7 +3312,7 @@ function createMarkers(locationsData, category) {
       arrowStyle: 0,
       minHeight: 286,
       maxHeight: 286,
-      minWidth: 780,
+      minWidth: 680,
       closeSrc: 'https://www.thistle.com/d/thistle/media/insta_map_assets/black/close-btn2x_f74344.png'
     });
   } else {
@@ -3442,7 +3442,7 @@ function createMarkers(locationsData, category) {
 
     google.maps.event.addListener(marker, 'click', function() {
       infoBubble.close();
-      infoBubble.setContent('<img src="' + this.image + '" style="max-width: 286px;" /><div class="info" style="display: inline; position: absolute; top: 0; width: 62%; padding: 15px;box-sizing: border-box;"><h4 class="amber">' + this.name + '</h4>' + '<p class="category">' + this.category + '</p>' + '<p><span class="amber">#</span>' + this.hashtag + '</p><p><span class="amber"> Posts: </span>  ' + this.posts + '</p><p class="copy">' + this.copy + '</p></div>');
+      infoBubble.setContent('<img src="' + this.image + '" style="max-width: 286px;" /><div class="info" style="display: inline; position: absolute; top: 0; width: 58%; padding: 15px;box-sizing: border-box;"><h4 class="amber">' + this.name + '</h4>' + '<p class="category">' + this.category + '</p>' + '<p><span class="amber">#</span>' + this.hashtag + '</p><p><span class="amber"> Posts: </span>  ' + this.posts + '</p><p class="copy">' + this.copy + '</p></div>');
       infoBubble.open(map, this);
 
       // add styles to the popup
